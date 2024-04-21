c7335edaf98fe736f862988b2c6437c22c24eab9
wander <kovacz@ukr.net>
2024-04-21 21:40:28 +0300

    Added `io` module

---
+++ flux-io/flux/io/print-test.cpp
@@ -0,0 +1,54 @@
+#include <flux/io.hpp>
+
+#include <catch2/catch.hpp>
+
+struct test_io_device {
+    using char_type = char;
+
+    std::string buffer;
+};
+
+template <typename Iter>
+constexpr void write(test_io_device& device, Iter begin, Iter end) noexcept {
+    device.buffer.assign(begin, end);
+}
+
+TEST_CASE("flux::io::print", "[flux-io/print.hpp]") {
+    using namespace flux;
+
+    SECTION("string literal") {
+        test_io_device device;
+
+        io::print(device, "test");
+        CHECK(device.buffer == std::string{"test"});
+    }
+
+    SECTION("integer values") {
+        test_io_device device;
+
+        int a = 1;
+        int b = 2;
+        io::print(device, a, b, 3);
+        CHECK(device.buffer == std::string{"123"});
+    }
+}
+
+TEST_CASE("flux::io::println", "[flux-io/print.hpp]") {
+    using namespace flux;
+
+    SECTION("string literal") {
+        test_io_device device;
+
+        io::println(device, "test");
+        CHECK(device.buffer == std::string{"test\n"});
+    }
+
+    SECTION("integer values") {
+        test_io_device device;
+
+        int a = 1;
+        int b = 2;
+        io::println(device, a, b, 3);
+        CHECK(device.buffer == std::string{"123\n"});
+    }
+}
---
+++ flux-meta/flux/meta/declval.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace flux::meta {
+
+// clang-format off
+// TODO:
+//  File a bug against the standard? The standard version always returns a
+//  reference, which means it does not work with guaranteed copy elision.
+template <typename T>
+constexpr auto declval() noexcept -> T;
+
+template <typename T>
+    requires(std::is_function_v<std::remove_cv_t<T>> or std::is_array_v<std::remove_cv_t<T>>)
+constexpr auto declval() noexcept -> T&&;
+// clang-format on
+
+} // namespace flux::meta
