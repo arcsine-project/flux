26367715e6a0f804d09e16fb9b704cd74b68043b
wander <kovacz@ukr.net>
2024-05-08 17:30:21 +0300

    Added configurations for different modules

---
+++ flux-config/flux/config/compiler.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#if defined(__clang__)
+#    define FLUX_CLANG_FULL_VERSION                                                                \
+        (__clang_major__ * 10000 + __clang_minor__ * 100 + __clang_patchlevel__)        
+//   For compatibility with existing code that compiles with MSVC,
+//   clang defines the _MSC_VER and _MSC_FULL_VER macros.
+#    if defined(_MSC_VER)
+#        define FLUX_MSC_FULL_VERSION (_MSC_FULL_VER)
+#    endif
+#else
+#    error "Compiler not supported."
+#endif
