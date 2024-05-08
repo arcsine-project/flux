2a388db3b14a3059546f0dd817c9a9012c4894d3
wander <kovacz@ukr.net>
2024-05-08 18:13:15 +0300

    Fixed formatting

--- flux-config/flux/config/compiler.hpp
+++ flux-config/flux/config/compiler.hpp
@@ -2,7 +2,7 @@
 
 #if defined(__clang__)
 #    define FLUX_CLANG_FULL_VERSION                                                                \
-        (__clang_major__ * 10000 + __clang_minor__ * 100 + __clang_patchlevel__)        
+        (__clang_major__ * 10000 + __clang_minor__ * 100 + __clang_patchlevel__)
 //   For compatibility with existing code that compiles with MSVC,
 //   clang defines the _MSC_VER and _MSC_FULL_VER macros.
 #    if defined(_MSC_VER)
