f59d878b618b02fe4672e393d1dce30885a59fb4
wander <kovacz@ukr.net>
2024-05-10 23:11:20 +0300

    Added more unit tests for `relocate` algorithms

--- flux-foundation/flux/foundation/memory/uninitialized_algorithms-test.cpp
+++ flux-foundation/flux/foundation/memory/uninitialized_algorithms-test.cpp
@@ -1,5 +1,6 @@
 #include <flux/foundation.hpp>
 
+#include <array>
 #include <catch2/catch.hpp>
 
 static_assert(flux::meta::trivially_relocatable<int>);
@@ -76,37 +77,119 @@ TEST_CASE("flux::fou::uninitialized_value_construct_n",
 TEST_CASE("flux::fou::uninitialized_relocate", "[flux-foundation/uninitialized_algorithms.hpp]") {
     using namespace flux::fou;
 
-    using InputIterator  = int*;
-    using OutputIterator = counted*;
-    auto const N         = 5;
-    int        values[N] = {1, 2, 3, 4, 5};
-    // clang-format off
-        alignas(counted)
-        std::byte pool[sizeof(counted) * N] = {};
-        counted*  counted_pointer           = reinterpret_cast<counted*>(pool);
-    // clang-format on
-    auto result = uninitialized_relocate(InputIterator(values), InputIterator(values + 1),
-                                         OutputIterator(counted_pointer));
-    CHECK(result == OutputIterator(counted_pointer + 1));
-    CHECK(counted::constructed == 1);
-    CHECK(counted::count == 1);
-    CHECK(counted_pointer[0].value == 1);
-    CHECK(values[0] == 0);
+    SECTION("compile-time") {
+        constexpr auto result = [] {
+            std::array<int, 5> v1 = {1, 2, 3, 4, 5};
+            std::array<int, 5> v2 = {};
+            uninitialized_relocate(v1.data(), v1.data() + v1.size(), v2.data());
+            return v2;
+        }();
+        STATIC_REQUIRE(result == std::array{1, 2, 3, 4, 5});
+    }
 
-    result = uninitialized_relocate(InputIterator(values + 1), InputIterator(values + N),
-                                    OutputIterator(counted_pointer + 1));
-    CHECK(result == OutputIterator(counted_pointer + N));
-    CHECK(counted::count == 5);
-    CHECK(counted::constructed == 5);
-    CHECK(counted_pointer[1].value == 2);
-    CHECK(counted_pointer[2].value == 3);
-    CHECK(counted_pointer[3].value == 4);
-    CHECK(counted_pointer[4].value == 5);
-    CHECK(values[1] == 0);
-    CHECK(values[2] == 0);
-    CHECK(values[3] == 0);
-    CHECK(values[4] == 0);
+    SECTION("memmove") {
+        std::array<int, 5> v1 = {5, 4, 3, 2, 1};
+        std::array<int, 5> v2 = {};
+        uninitialized_relocate(v1.data(), v1.data() + v1.size(), v2.data());
+        CHECK(v2 == std::array{5, 4, 3, 2, 1});
+    }
 
-    destroy_range(counted_pointer, counted_pointer + N);
-    CHECK(counted::count == 0);
+    SECTION("run-time") {
+        counted::reset();
+
+        using InputIterator  = int*;
+        using OutputIterator = counted*;
+
+        int const N         = 5;
+        int       values[N] = {1, 2, 3, 4, 5};
+
+        alignas(counted) std::byte pool[sizeof(counted) * N] = {};
+        counted*                   counted_pointer           = reinterpret_cast<counted*>(pool);
+
+        auto result = uninitialized_relocate(InputIterator(values), InputIterator(values + 1),
+                                             OutputIterator(counted_pointer));
+        CHECK(result == OutputIterator(counted_pointer + 1));
+        CHECK(counted::constructed == 1);
+        CHECK(counted::count == 1);
+        CHECK(counted_pointer[0].value == 1);
+        CHECK(values[0] == 0);
+
+        result = uninitialized_relocate(InputIterator(values + 1), InputIterator(values + N),
+                                        OutputIterator(counted_pointer + 1));
+        CHECK(result == OutputIterator(counted_pointer + N));
+        CHECK(counted::count == 5);
+        CHECK(counted::constructed == 5);
+        CHECK(counted_pointer[1].value == 2);
+        CHECK(counted_pointer[2].value == 3);
+        CHECK(counted_pointer[3].value == 4);
+        CHECK(counted_pointer[4].value == 5);
+        CHECK(values[1] == 0);
+        CHECK(values[2] == 0);
+        CHECK(values[3] == 0);
+        CHECK(values[4] == 0);
+
+        destroy_range(counted_pointer, counted_pointer + N);
+        CHECK(counted::count == 0);
+    }
+}
+
+TEST_CASE("flux::fou::uninitialized_relocate_no_overlap",
+          "[flux-foundation/uninitialized_algorithms.hpp]") {
+    using namespace flux::fou;
+
+    SECTION("compile-time") {
+        constexpr auto result = [] {
+            std::array<int, 5> v1 = {1, 2, 3, 4, 5};
+            std::array<int, 5> v2 = {};
+            uninitialized_relocate_no_overlap(v1.data(), v1.data() + v1.size(), v2.data());
+            return v2;
+        }();
+        STATIC_REQUIRE(result == std::array{1, 2, 3, 4, 5});
+    }
+
+    SECTION("memcpy") {
+        std::array<int, 5> v1 = {5, 4, 3, 2, 1};
+        std::array<int, 5> v2 = {};
+        uninitialized_relocate_no_overlap(v1.data(), v1.data() + v1.size(), v2.data());
+        CHECK(v2 == std::array{5, 4, 3, 2, 1});
+    }
+
+    SECTION("run-time") {
+        counted::reset();
+
+        using InputIterator  = int*;
+        using OutputIterator = counted*;
+
+        int const N         = 5;
+        int       values[N] = {1, 2, 3, 4, 5};
+
+        alignas(counted) std::byte pool[sizeof(counted) * N] = {};
+        counted*                   counted_pointer           = reinterpret_cast<counted*>(pool);
+
+        auto result = uninitialized_relocate_no_overlap(
+                InputIterator(values), InputIterator(values + 1), OutputIterator(counted_pointer));
+        CHECK(result == OutputIterator(counted_pointer + 1));
+        CHECK(counted::constructed == 1);
+        CHECK(counted::count == 1);
+        CHECK(counted_pointer[0].value == 1);
+        CHECK(values[0] == 0);
+
+        result = uninitialized_relocate_no_overlap(InputIterator(values + 1),
+                                                   InputIterator(values + N),
+                                                   OutputIterator(counted_pointer + 1));
+        CHECK(result == OutputIterator(counted_pointer + N));
+        CHECK(counted::count == 5);
+        CHECK(counted::constructed == 5);
+        CHECK(counted_pointer[1].value == 2);
+        CHECK(counted_pointer[2].value == 3);
+        CHECK(counted_pointer[3].value == 4);
+        CHECK(counted_pointer[4].value == 5);
+        CHECK(values[1] == 0);
+        CHECK(values[2] == 0);
+        CHECK(values[3] == 0);
+        CHECK(values[4] == 0);
+
+        destroy_range(counted_pointer, counted_pointer + N);
+        CHECK(counted::count == 0);
+    }
 }
